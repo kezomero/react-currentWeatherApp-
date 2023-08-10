30884d3ec0b69214ba08faf94d950dac2f29a347
sir derrick <dimatata01@gmail.com>
2023-08-10 17:37:02 +0300

    ..

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Home from './screens/Home';
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 export default function App() {
+  const Stack=createStackNavigator()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{flex:1}}>
+      <View style={{flex:1}}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name='Home' component={Home} options={{headerShown:false}}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </View>
+    </SafeAreaView>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
---
+++ screens/Home.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View style={{flex:1}}>
+      <Text style={{alignItems:'center',justifyContent:'center',fontSize:50}}>Home</Text>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
